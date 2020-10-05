0d10cb827fd7aa1605501dd724be190be3c8f107
Lasse Nordahl <lasseanordahl@gmail.com>
2020-10-05 14:01:38 -0700

    updated wave resources

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -12,6 +12,8 @@ import bubbles from "assets/images/bubbles.png"
 import mentorship from "assets/images/mentorship.png"
 import butterflyOne from "assets/images/butterflyOne.png"
 import butterflyTwo from "assets/images/butterflyTwo.png"
+import sinWaveOne from "assets/images/white_sinewave_1.png"
+import sinWaveTwo from "assets/images/white_sinewave_2.png"
 
 function Home() {
   return (
@@ -79,18 +81,7 @@ function Home() {
             </div>
           </div>
         </div>
-        <svg
-          className="wave-transition"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-          preserveAspectRatio="none"
-        >
-          <path
-            fill="#fcfcfc"
-            fill-opacity="1"
-            d="M0,224L48,208C96,192,192,160,288,138.7C384,117,480,107,576,128C672,149,768,203,864,208C960,213,1056,171,1152,149.3C1248,128,1344,128,1392,128L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-          ></path>
-        </svg>
+        <img className="sin-wave sin-wave--one" src={sinWaveOne}/>
       </section>
       <section id="info">
         <img src={bubbles} />
@@ -132,17 +123,7 @@ function Home() {
         <img className="mentors-butterfly --two" src={butterflyTwo} />
       </section>
       <section id="faq">
-        <svg
-          className="wave-transition"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
-          <path
-            fill="#fcfcfc"
-            fill-opacity="1"
-            d="M0,96L60,101.3C120,107,240,117,360,112C480,107,600,85,720,74.7C840,64,960,64,1080,80C1200,96,1320,128,1380,144L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-          ></path>
-        </svg>
+        <img className="sin-wave sin-wave--two" src={sinWaveTwo}/>
         <Fade duration={1000}>
           <h2>FAQ</h2>
           <div>

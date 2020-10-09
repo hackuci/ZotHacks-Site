76ff6d82b1484eaf1be0a417e7d0e198018274f5
Lasse Nordahl <lasseanordahl@gmail.com>
2020-10-08 23:07:14 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12bb54bb70b2c8d0082baaae4741cb8e.js"
+  "/precache-manifest.f4e50fea429532b7af94be27dace5a77.js"
 );
 
 self.addEventListener('message', (event) => {

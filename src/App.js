82fc2ac93c36ddb4bc5e908f1ce7032bfdf8c09c
Lasse Nordahl <lasseanordahl@gmail.com>
2020-11-11 21:29:01 -0800

    hmm

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/schedule" component={Schedule} />
           <Route exact path="/starter-packs" component={StarterPacks} />
+        
+          <Route path='/github' component={() => { 
+            window.location.href = 'https://education.github.com/discount_requests/student_application?utm_source=2020-11-13-ZotHacks2020'; 
+            return null;
+          }}/>
         </Switch>
       </HashRouter>
     </div>

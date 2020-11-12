b99f4bbf86155b09f8e3a3411183f0730689fe45
Lasse Nordahl <lasseanordahl@gmail.com>
2020-11-11 21:23:02 -0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch, BrowserRouter } from "react-router-dom"
+import { Route, Switch, HashRouter } from "react-router-dom"
 import history from "./history"
 import "./App.scss"
 
@@ -10,14 +10,14 @@ import { Nav } from "app/components"
 function App() {
   return (
     <div className="App">
-      <BrowserRouter history={history} basename="/Zothacks-Site">
+      <HashRouter history={history} basename="/Zothacks-Site">
         <Nav history={history}></Nav>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/schedule" component={Schedule} />
           <Route exact path="/starter-packs" component={StarterPacks} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }

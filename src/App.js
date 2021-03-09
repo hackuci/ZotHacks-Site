8e084e7f3325bda706ffa984bfca1db1539740f2
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-03-08 18:23:24 -0800

    Test SPA React Routing
    By utilizing a custom script in the 404 page, we may be able to achieve routing in GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch, HashRouter } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import history from "./history"
 import "./App.scss"
 
@@ -10,19 +10,17 @@ import { Nav } from "app/components"
 function App() {
   return (
     <div className="App">
-      <HashRouter history={history} basename="/Zothacks-Site">
-        <Nav history={history}></Nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/schedule" component={Schedule} />
-          <Route exact path="/starter-packs" component={StarterPacks} />
-        
-          <Route path='/github' component={() => { 
-            window.location.href = 'https://education.github.com/discount_requests/student_application?utm_source=2020-11-13-ZotHacks2020'; 
-            return null;
-          }}/>
-        </Switch>
-      </HashRouter>
+      <Nav history={history}></Nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/schedule" component={Schedule} />
+        <Route exact path="/starter-packs" component={StarterPacks} />
+      
+        <Route path='/github' component={() => { 
+          window.location.href = 'https://education.github.com/discount_requests/student_application?utm_source=2020-11-13-ZotHacks2020'; 
+          return null;
+        }}/>
+      </Switch>
     </div>
   )
 }
